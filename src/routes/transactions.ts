8d21e5dc7329afbafb23bdb01ce5abf944fdffef
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2025-04-10 19:47:25 -0300

    implement transaction listing endpoint

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,6 +3,25 @@ import { knex } from "../database";
 import { z } from "zod";
 
 export function transactionsRoutes(app: FastifyInstance) {
+  app.get("/", async (req, reply) => {
+    const transactions = await knex("transactions").select("*");
+    return {
+      transactions,
+    };
+  });
+
+  app.get("/:id", async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+    const { id } = getTransactionsParamsSchema.parse(request.params);
+    const transaction = await knex("transactions").where("id", id).first();
+    
+    return {
+      transaction,
+    };
+  });
+
   app.post("/", async (req, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
