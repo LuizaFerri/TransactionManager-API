57e267e2d920cfe413a8669212cb401ff2da64ba
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2025-04-11 20:55:39 -0300

    add automated tests for transaction creation

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,5 @@
-import fastify from "fastify";
-import cookie from "@fastify/cookie";
+import { app } from "./app";
 import { env } from "./env";
-import { transactionsRoutes } from "./routes/transactions";
-const app = fastify();
-
-app.register(transactionsRoutes, {
-  prefix: "transactions",
-});
-
-app.register(cookie);
 
 app.listen({ port: env.PORT }, () => {
   console.log(`HTTP Server Running! http://localhost:${env.PORT}`);
